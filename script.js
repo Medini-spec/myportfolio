a68728a9f0f5938ab02d1511dfb75f5aef8c7fac
Medini-spec <73520508+Medini-spec@users.noreply.github.com>
2025-04-01 10:30:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
 	});   
         //typing animation script 
         var typed = new Typed(".typing",{
-       	strings: ["Creative Designer"],
+       	strings: ["Creative Developer"],
        	typeSpeed : 100,
        	backSpeed : 60,
        	loop : true 
